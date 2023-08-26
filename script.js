12eb4d48412c54f69b3bdf6fb32947ce653a6ea0
Krishan Chakroborty <138010864+krisHChakroborty@users.noreply.github.com>
2023-08-26 12:44:31 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,132 @@
 
-let table = parseInt(prompt("Enter Your Number!"));
 
 
-for(let i = 1; i<=10; i++){
+
+// --------------------------------------------------Mulification Table------------------------------------------------------
+
+
+
+
+// let table = parseInt(prompt("Enter Your Number!"));
+
+
+// for(let i = 1; i<=10; i++){
 
     
-    document.write("<br>"+"<br>"+ `${table} x ${i} = ${table*i}`)
-}
+//     document.write("<br>"+"<br>"+ `${table} x ${i} = ${table*i}`)
+// }
+
+
+
+
+
+// --------------------------------------------------Mulification Table------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function About(name,pincode,number){
+
+    
+
+//     document.write(`My name is ${name},My pincode is ${pincode}, My mobile number is ${number}`+"<br>");
+   
+    
+// }
+
+
+
+// About("krishan", 788819,9101759186)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// function About(nam,pincode,number){
+
+//     return nam+pincode+number;
+
+   
+// }
+
+// let about =  About("rahul" , "raj" , "shyam")
+
+// document.write(about);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+    //////////////////// add
+
+// let yourarray = ["ram", "shyam" , "jadu" , "madhu"]
+
+// yourarray.push("rakesh","rohan")
+
+// document.write(yourarray);
+//////////////////// add///////////////////
+
+
+
+
+
+/////////////////// remove first 2 words
+// let yourarray = ["ram", "shyam" , "jadu" , "madhu"]
+
+// yourarray.shift()
+
+// yourarray.shift()
+
+// document.write(yourarray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
