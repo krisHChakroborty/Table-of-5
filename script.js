d89656d27068db070ceea0a1066134b2683570ae
Krishan Chakroborty <138010864+krisHChakroborty@users.noreply.github.com>
2023-08-27 16:38:46 +0530

    new

--- script.js
+++ script.js
@@ -1,165 +1,38 @@
+// 01. arr = ['sdfd', 'tr', '23'] -- insert "hi" on secend positin
 
 
 
 
-// --------------------------------------------------Mulification Table------------------------------------------------------
+// let arr = ["sdfd" , "tr" , 23]
+
+// arr.splice(1,0,"hi")
+
+// document.write(arr);
 
 
 
 
-// let table = parseInt(prompt("Enter Your Number!"));
 
 
-// for(let i = 1; i<=10; i++){
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    
-//     document.write("<br>"+"<br>"+ `${table} x ${i} = ${table*i}`)
+
+
+
+// 02. friends = ["don", "srk", "salman"] -- check if srk is present if present "I am SrK"
+
+
+// let friends = ["don", "srk", "salman"]
+ 
+// let a = friends.includes("srk")
+
+
+// document.write(a);
+
+// document.write("<br><br>")
+
+// if(a==true){
+//     document.write("i am Srk!");
+// }else{
+//     document.write("No record found");
 // }
-
-
-
-
-
-// --------------------------------------------------Mulification Table------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function About(name,pincode,number){
-
-    
-
-//     document.write(`My name is ${name},My pincode is ${pincode}, My mobile number is ${number}`+"<br>");
-   
-    
-// }
-
-
-
-// About("krishan", 788819,9101759186)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-// function About(nam,pincode,number){
-
-//     return nam+pincode+number;
-
-   
-// }
-
-// let about =  About("rahul" , "raj" , "shyam")
-
-// document.write(about);
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-    //////////////////// add
-
-// let yourarray = ["ram", "shyam" , "jadu" , "madhu"]
-
-// yourarray.push("rakesh","rohan")
-
-// document.write(yourarray);
-//////////////////// add///////////////////
-
-
-
-
-
-
-
-
-
-
-/////////////////// remove first 2 words
-
-// let yourarray = ["ram", "shyam" , "jadu" , "madhu"]
-
-// yourarray.shift()
-
-// yourarray.shift()
-
-// document.write(yourarray);
-
-/////////////////// remove first 2 words
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// let yourarray = ["ram", "shyam" , "jadu" , "madhu"]
-
-// for(let i = 0; i<yourarray.length; i++){
-
-
-//     document.write(yourarray[i]+"<br>" );
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
